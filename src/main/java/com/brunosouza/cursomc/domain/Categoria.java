04a141ce96b166de06b0670c3f0874a805baa436
Bruno Souza <bruno1994deilson@gmail.com>
2021-03-23 09:23:31 -0300

    Criando operacao de instanciacao

--- src/main/java/com/brunosouza/cursomc/domain/Categoria.java
+++ src/main/java/com/brunosouza/cursomc/domain/Categoria.java
@@ -12,7 +12,7 @@ public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
