acf5d667b8cc3cd4c1ce8ba8cb053bd02de572e8
Bruno Souza <bruno1994deilson@gmail.com>
2021-04-13 17:24:21 -0300

    Validacao customizada: email nao repetido na insercao de cliente

--- src/main/java/com/brunosouza/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/brunosouza/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.brunosouza.cursomc.domain.Cliente;
+import com.brunosouza.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
